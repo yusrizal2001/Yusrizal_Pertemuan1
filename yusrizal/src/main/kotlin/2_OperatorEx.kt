0b7efc3205fa691748f4fd244d459a33c4a405e3
yusrizal2001 <yusrizal10102001@gmail.com>
2022-03-11 16:10:53 +0700

    Percobaan literal

---
+++ yusrizal/src/main/kotlin/2_OperatorEx.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var a = 7
+    var b = 6
+    var c = (a++ + b++)/2.0
+    println(c+b)
+    var dinamis = "hore"
+    var statis = 2.0
+    println(statis)
+    statis = 3.0
+    statis = 5.9
+    val PHI = 3.14*12
+    println(PHI)
+
+}
---
+++ yusrizal/src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+//fungsi main adalah fungsi untuk menampilkan di eksekusi
+fun main(args: Array<String>) {
+    //untuk mencetak string ke layar
+    println("Hello World!")
+}
