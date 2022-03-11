9b5fdf1cb4d81030556e539609a50629185b5765
yusrizal2001 <yusrizal10102001@gmail.com>
2022-03-11 11:31:07 +0700

    Initial project pertemuan1

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+//fungsi main adalah fungsi untuk menampilkan hasil di eksekusi
+fun main(args: Array<String>) {
+    //untuk mencetak string ke layar
+    println("Hello,ini Program Pertama Saya di Kotlin,!")
+}
