e0815a3a339e9900b8bc3f34aa6fb28cbae8ad24
yusrizal2001 <yusrizal10102001@gmail.com>
2022-03-11 16:33:02 +0700

    Variabel

---
+++ yusrizal/src/main/kotlin/4_Variabel.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>){
+    var dynamicVar = 2
+    println("Contoh dynamic variabel = " + dynamicVar)
+
+    var staticVar: Char = 'A'
+    println("contoh static variabel=" + staticVar)
+}
