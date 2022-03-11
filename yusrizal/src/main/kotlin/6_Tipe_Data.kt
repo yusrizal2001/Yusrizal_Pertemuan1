7fafb759d506305eda373d6244d5bffffa763e73
yusrizal2001 <yusrizal10102001@gmail.com>
2022-03-11 17:03:33 +0700

    Percobaan literal

---
+++ yusrizal/src/main/kotlin/6_Tipe_Data.kt
@@ -0,0 +1,26 @@
+//tipe data dikotlin terdiri dari byte, Short, Int dan Long untuk bilangan bulat (Number), Double
+//dan Float untuk bilangan desimal atau real (Floating-Point),Char untuk karalter, Boolean untuk
+//Kondisional (true/false), String untuk array karakter, Array yang meruapakan koleksi data dengan
+//tipe data yang homogen, collection dan range
+
+//Contoh kode program menggunakan tipe data bilangan bulat:
+fun main(args: Array<String>){
+    val minByte: Byte = -128
+    val maxByte: Byte = 127 // 8bit
+    val minShort: Short = -32768
+    val maxShort: Short = 32767 // 16bit
+    val minInt: Int = -2147483648
+    val maxInt: Int = -2147483647 // 32bit
+    val minLong: Long = -9223372036854775807
+    val maxLong: Long = 9223372036854775807 //64bit
+
+    println("minByte:" + minByte)
+    println("maxByte:" + maxByte)
+    println("minShort:" + minShort)
+    println("maxShort:" + maxShort)
+    println("minInt:" + minInt)
+    println("maxInt:" + maxInt)
+    println("minLong:" + minLong)
+    println("maxLong:" + maxLong)
+
+}
